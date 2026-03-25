72833e0a9656efb7364e258dcf2013dbf269978e
25bcab64-cyber <25bcab64@kristujayanti.com>
2026-03-25 22:02:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ app.post("/contact", (req, res) => {
 });
 
 // Start server
+app.get('/', (req, res) => {
+  res.send('Backend Running 🚀');
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
