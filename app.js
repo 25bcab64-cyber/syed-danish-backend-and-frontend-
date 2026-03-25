81c78b1f99e0007125c36a5c7078cfa1ec4c4638
25bcab64-cyber <25bcab64@kristujayanti.com>
2026-03-25 22:30:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
-const cors = require("cors");
-const path = require("path");   // ⭐ ADD THIS
+const cors = require("cors");   // ⭐ ADD THIS
 
 const app = express();
 
